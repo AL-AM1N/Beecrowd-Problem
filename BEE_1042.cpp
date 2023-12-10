01154b72d14922bde07286f11efd1e47286acdfd
AL-AM1N <152994093+AL-AM1N@users.noreply.github.com>
2023-12-10 21:45:28 +0600

    Simple Sort

---
+++ BEE_1042.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a<b && a<c){
+        cout<<a<< endl;
+        if(b<c){
+            cout << b << endl << c << endl;
+        }
+        else{
+            cout << c<< endl << b << endl;
+        }
+    }
+    else if(b<a && b<c){
+        cout<<b<< endl;
+        if(a<c){
+            cout << a << endl << c << endl;
+        }
+        else{
+            cout << c << endl << a << endl;
+        }
+    }
+    else if(c<a && c<b){
+        cout<<c<< endl;
+        if(b<a){
+            cout << b << endl << a << endl;
+        }
+        else{
+            cout << a << endl << b << endl;
+        }
+    }
+    cout << endl;
+    cout << a << endl << b << endl << c << endl;
+    return 0;
+}
